5efed21798f803100ef4cecd732e8830c2d03e76
alessandraR <2006225@newton.pe>
2022-03-21 09:45:08 -0500

    First Commit

---
+++ src/main/java/org/example/PrimaryController.java
@@ -0,0 +1,17 @@
+package org.example;
+
+import java.io.IOException;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+
+public class PrimaryController {
+
+    @FXML
+    private void switchToSecondary() throws IOException {
+        App.setRoot("secondary");
+    }
+
+    public void cancelButton(ActionEvent actionEvent) {
+    }
+}
