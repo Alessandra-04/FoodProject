d065ba6c0afe2d3714d837287e9479368627f159
alessandraR <2006225@newton.pe>
2022-04-03 16:19:56 -0500

    First Commit

--- src/main/java/org/example/SecondaryController.java
+++ src/main/java/org/example/SecondaryController.java
@@ -2,11 +2,28 @@ package org.example;
 
 import java.io.IOException;
 import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.layout.HBox;
 
 public class SecondaryController {
+
+    public Button btnProducts;
+    public HBox hboxtitle;
 
     @FXML
     private void switchToPrimary() throws IOException {
         App.setRoot("primary");
     }
+
+    @FXML
+    private void showProductsDashboard() throws IOException {
+
+        if (hboxtitle.isVisible()) {
+            hboxtitle.setVisible(false);
+        }else{
+            hboxtitle.setVisible(true);
+        }
+    }
+
+
 }
