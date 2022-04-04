efdca42818ac40a7de1a0a9472307c23fd242c96
alessandraR <2006225@newton.pe>
2022-04-04 09:54:00 -0500

    First Commit

--- src/main/java/org/example/SecondaryController.java
+++ src/main/java/org/example/SecondaryController.java
@@ -1,33 +1,107 @@
 package org.example;
 
+import java.awt.event.ActionEvent;
 import java.io.IOException;
+import java.net.URL;
+import java.util.ResourceBundle;
+
 import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.geometry.Insets;
 import javafx.scene.control.Button;
-import javafx.scene.layout.HBox;
+import javafx.scene.control.Label;
+import javafx.scene.layout.*;
+import javafx.scene.paint.Color;
 
-public class SecondaryController {
+public class SecondaryController implements Initializable {
 
-    public Button btnProducts;
-    public HBox hboxtitle;
-    public Button btnOrders;
-    public Button btnInventory;
-    public Button btnCustomers;
-    public Button btnEmployees;
+    @FXML
+    private Button btnCustomers;
+
+    @FXML
+    private Button btnEmployees;
+
+    @FXML
+    private Button btnInventory;
+
+    @FXML
+    private Button btnLogout;
+
+    @FXML
+    private Button btnOrders;
+
+    @FXML
+    private Button btnOverview;
+
+    @FXML
+    private Button btnProducts;
+
+    @FXML
+    private Pane pnlTitle;
+
+    @FXML
+    private Label lblTitleUnit;
+
+    // grid panes
+
+    @FXML
+    private GridPane pnlCustomers;
+
+    @FXML
+    private GridPane pnlEmployees;
+
+    @FXML
+    private GridPane pnlInventory;
+
+    @FXML
+    private GridPane pnlOrders;
+
+    @FXML
+    private GridPane pnlOverview;
+
+    @FXML
+    private GridPane pnlProducts;
 
     @FXML
     private void switchToPrimary() throws IOException {
         App.setRoot("primary");
     }
 
-    @FXML
-    private void showProductsDashboard() throws IOException {
-
-        if (hboxtitle.isVisible()) {
-            hboxtitle.setVisible(false);
-        }else{
-            hboxtitle.setVisible(true);
-        }
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
     }
 
+    @FXML
+    private void handleClinks(javafx.event.ActionEvent event) {
+        if (event.getSource() == btnOverview) {
+            lblTitleUnit.setText("OVERVIEW");
+            pnlTitle.setBackground(new Background(new BackgroundFill(Color.rgb(159, 231, 245), CornerRadii.EMPTY, Insets.EMPTY)));
+            pnlOverview.toFront();
 
+        } else if (event.getSource() == btnOrders) {
+            lblTitleUnit.setText("ORDERS");
+            pnlTitle.setBackground(new Background(new BackgroundFill(Color.rgb(157, 255, 232), CornerRadii.EMPTY, Insets.EMPTY)));
+            pnlOrders.toFront();
+
+        } else if (event.getSource() == btnInventory) {
+            lblTitleUnit.setText("INVENTORY");
+            pnlTitle.setBackground(new Background(new BackgroundFill(Color.rgb(85, 168, 254), CornerRadii.EMPTY, Insets.EMPTY)));
+            pnlInventory.toFront();
+
+        } else if (event.getSource() == btnProducts) {
+            lblTitleUnit.setText("PRODUCTS");
+            pnlTitle.setBackground(new Background(new BackgroundFill(Color.rgb(85, 168, 254), CornerRadii.EMPTY, Insets.EMPTY)));
+            pnlProducts.toFront();
+
+        } else if (event.getSource() == btnCustomers) {
+            lblTitleUnit.setText("CUSTOMERS");
+            pnlTitle.setBackground(new Background(new BackgroundFill(Color.rgb(85, 168, 254), CornerRadii.EMPTY, Insets.EMPTY)));
+            pnlCustomers.toFront();
+
+        } else if (event.getSource() == btnEmployees) {
+            lblTitleUnit.setText("EMPLOYEES");
+            pnlTitle.setBackground(new Background(new BackgroundFill(Color.rgb(85, 168, 254), CornerRadii.EMPTY, Insets.EMPTY)));
+            pnlEmployees.toFront();
+        }
+    }
 }
