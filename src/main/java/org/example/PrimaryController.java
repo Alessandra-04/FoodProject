444f01872a858d3cf86f07374bfd1259ea36c0a1
alessandraR <2006225@newton.pe>
2022-04-11 10:06:59 -0500

    First Commit

--- src/main/java/org/example/PrimaryController.java
+++ src/main/java/org/example/PrimaryController.java
@@ -1,15 +1,35 @@
 package org.example;
 
+import java.io.File;
 import java.io.IOException;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import org.bytedeco.tesseract.Tesseract;
 
 public class PrimaryController {
 
     @FXML
     private void switchToSecondary() throws IOException {
         App.setRoot("secondary");
+
+        Tesseract tesseract = new Tesseract();
+        try {
+
+            tesseract.setDatapath("D:/Tess4J/tessdata");
+
+            // the path of your tess data folder
+            // inside the extracted file
+            String text
+                    = tesseract.doOCR(new File("test1.jpg"));
+
+            // path of your image file
+            System.out.print(text);
+        }
+        catch (TesseractException e) {
+            e.printStackTrace();
+        }
+
     }
 
     public void cancelButton(ActionEvent actionEvent) {
