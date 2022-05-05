12461fd2efe9e5af1d87a8f3c8c3f7e199d84156
alessandraR <2006225@newton.pe>
2022-05-05 15:52:21 -0500

    First Commit

--- src/main/java/org/example/PrimaryController.java
+++ src/main/java/org/example/PrimaryController.java
@@ -1,8 +1,13 @@
 package org.example;
 
 import java.io.File;
+import java.io.FileReader;
 import java.io.IOException;
 
+import Staff.java.Staff;
+import Staff.json.Staff;
+import com.google.gson.Gson;
+import com.google.gson.JsonElement;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 
@@ -11,6 +16,16 @@ public class PrimaryController {
     @FXML
     private void switchToSecondary() throws IOException {
         App.setRoot("secondary");
+
+        Gson gson = new Gson();
+
+        Staff obj = new Staff();
+
+        // 1. Java object to JSON file
+        gson.toJson(obj, new FileWriter("C:\\projects\\staff.json"));
+
+        // 2. Java object to JSON string
+        String jsonInString = gson.toJson(obj);
 
     }
 }
--- src/main/java/org/example/SecondaryController.java
+++ src/main/java/org/example/SecondaryController.java
@@ -1,10 +1,10 @@
 package org.example;
 
-import java.awt.event.ActionEvent;
 import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
 
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
@@ -125,5 +125,8 @@ public class SecondaryController implements Initializable {
             pnlTitle.setBackground(new Background(new BackgroundFill(Color.rgb(85, 168, 254), CornerRadii.EMPTY, Insets.EMPTY)));
             pnlEmployees.toFront();
         }
+    }
+
+    public void switchToPrimary(ActionEvent actionEvent) {
     }
 }
